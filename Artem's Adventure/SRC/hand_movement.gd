89d8f8fa03365985a48415c12d4752d2a15d571f
Gauliux <artyomprokopenko.ru@gmail.com>
2022-10-26 01:55:24 +0300

    Beginning.

---
+++ Artem's Adventure/SRC/hand_movement.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (int) var speed = 3000
+
+var velocity = Vector2()
+
+func _ready():
+	self.hide()
+
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("hand_activate"):
+		self.show()
+		look_at(get_global_mouse_position())
+		velocity = Vector2(speed, 0).rotated(rotation)
+
+func _unhandled_input(event):
+	self.hide()
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
