d8a7d4081089e0fc085feab3ac7353bf3049b95a
Gauliux <artyomprokopenko.ru@gmail.com>
2022-11-01 21:55:47 +0300

    Little changes.

--- Artem's Adventure/SRC/hand_movement.gd
+++ Artem's Adventure/SRC/hand_movement.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
 
-export (int) var speed = 500
+export (int) var speed = 1000
 export (int) var move_back_speed = 200
 
 var velocity = Vector2()
 var pre_position = position
 
 func _ready():
-	#self.hide()
-	self.show()
+	self.hide()
+	#self.show()
 	#pre_position = position
 
 func _process(delta):
@@ -21,7 +21,7 @@ func _process(delta):
 		pre_position = position
 		#print("pressed")
 	if Input.is_action_pressed("hand_activate"):
-		#self.show()
+		self.show()
 		velocity = Vector2(speed, 0).rotated(rotation)
 		
 	if Input.is_action_just_released("hand_activate"):
@@ -29,8 +29,8 @@ func _process(delta):
 		#print("released")
 
 func _unhandled_input(event):
-	#self.hide()
-	self.show()
+	self.hide()
+	#self.show()
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity)
