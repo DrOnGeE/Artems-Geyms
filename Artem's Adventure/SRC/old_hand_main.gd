efc020b232270e80a73bfb216a2b509e3ce303dc
Gauliux <artyomprokopenko.ru@gmail.com>
2022-11-08 12:39:29 +0300

    Changing hand behavior - in progress.

--- Artem's Adventure/SRC/old_hand_main.gd
+++ Artem's Adventure/SRC/old_hand_main.gd
@@ -1,13 +1,10 @@
 extends KinematicBody2D
 
-export (int) var speed = 30
+export (int) var speed = 1000
 
 var velocity = Vector2()
 var pre_position = position
 var hand_vision = 1
-
-var can_move = false
-var direction
 
 #var bullet_speed = 2000
 var bullet = preload("res://OBJ/BULLET.tscn")
@@ -15,13 +12,13 @@ var can_fire = true
 var rate_of_fire = 0.4
 
 func _ready():
-	#self.hide()
-	self.show()
+	self.hide()
+	#self.show()
 	pass
 	
 func _unhandled_input(event):
-	#self.hide()
-	self.show()
+	self.hide()
+	#self.show()
 	pass
 
 func _process(delta):
@@ -31,42 +28,29 @@ func _process(delta):
 	
 
 func _physics_process(delta):	
-	#velocity = move_and_slide(velocity)
-	if (can_move == true):
-		Move()
+	velocity = move_and_slide(velocity)
 	pass
 
 func ControlLoop():
-	if (can_move == false):
-		look_at(get_global_mouse_position())
+	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("hand_activate"):
 		pre_position = position
-		direction = get_global_mouse_position()
-		look_at(direction)
-		can_move = true
-		pass
-		
+		#print("pressed")
 	if Input.is_action_pressed("hand_activate"):
-		pass
-
-
-#FIRE
+		self.show()
+		can_fire = false
+		velocity = Vector2(speed, 0).rotated(rotation)
 	if Input.is_action_pressed("hand_shoot") and can_fire == true:
 		can_fire = false
-		#HandVision()
+		HandVision()
 		Fire()
 		yield(get_tree().create_timer(rate_of_fire), "timeout")
 		can_fire = true
 	if Input.is_action_just_released("hand_activate"):
-		#position = pre_position
+		position = pre_position
 		can_fire = true
 		#print("released")
-		pass
-	pass
-
-func Move():
-	velocity = Vector2(speed, 0).rotated(rotation)
-	position+=velocity
+	
 	pass
 
 func HandVision():
@@ -81,9 +65,3 @@ func Fire():
 	bullet_instance.rotation_degrees = rotation_degrees
 	get_tree().get_root().call_deferred("add_child", bullet_instance)
 	pass
-
-##NOT CORRECT!!!
-func _on_HandLimits_area_entered(area):
-	can_move = false
-	position = pre_position
-	pass # Replace with function body.
