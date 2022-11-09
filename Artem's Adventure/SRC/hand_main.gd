183c42c0f58bddb1cde2ff0aea6c0f87da8bf213
Gauliux <artyomprokopenko.ru@gmail.com>
2022-11-09 11:41:48 +0300

    Changing hand behavior - in progress #2

--- Artem's Adventure/SRC/hand_main.gd
+++ Artem's Adventure/SRC/hand_main.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 30
+export (int) var speed = 10
 
 var velocity = Vector2()
 var pre_position = position
@@ -9,14 +9,16 @@ var hand_vision = 1
 var can_move = false
 var direction
 
-#var bullet_speed = 2000
+#bullet
 var bullet = preload("res://OBJ/BULLET.tscn")
 var can_fire = true
 var rate_of_fire = 0.4
+#######
 
 func _ready():
 	#self.hide()
 	self.show()
+	pre_position = position
 	pass
 	
 func _unhandled_input(event):
@@ -30,7 +32,7 @@ func _process(delta):
 	pass
 	
 
-func _physics_process(delta):	
+func _physics_process(delta):
 	#velocity = move_and_slide(velocity)
 	if (can_move == true):
 		Move()
@@ -39,27 +41,32 @@ func _physics_process(delta):
 func ControlLoop():
 	if (can_move == false):
 		look_at(get_global_mouse_position())
+		
 	if Input.is_action_just_pressed("hand_activate"):
-		pre_position = position
+		#pre_position = position
 		direction = get_global_mouse_position()
 		look_at(direction)
 		can_move = true
+		can_fire = false
 		pass
 		
 	if Input.is_action_pressed("hand_activate"):
+		direction = get_global_mouse_position()
+		look_at(direction)
 		pass
 
 
 #FIRE
 	if Input.is_action_pressed("hand_shoot") and can_fire == true:
-		can_fire = false
+		#can_fire = false
 		#HandVision()
 		Fire()
-		yield(get_tree().create_timer(rate_of_fire), "timeout")
-		can_fire = true
+		#yield(get_tree().create_timer(rate_of_fire), "timeout")
+		#can_fire = true
+		pass
 	if Input.is_action_just_released("hand_activate"):
 		#position = pre_position
-		can_fire = true
+		#can_fire = true
 		#print("released")
 		pass
 	pass
@@ -75,15 +82,25 @@ func HandVision():
 	self.hide()
 
 func Fire():
+	can_fire = false
+	#########
 	get_node("TurnAxis/BulletPoint").rotation = get_angle_to(get_global_mouse_position())
 	var bullet_instance = bullet.instance()
 	bullet_instance.position = get_node("TurnAxis/BulletPoint").get_global_position()
 	bullet_instance.rotation_degrees = rotation_degrees
 	get_tree().get_root().call_deferred("add_child", bullet_instance)
+	#########
+	yield(get_tree().create_timer(rate_of_fire), "timeout")
+	can_fire = true
 	pass
 
-##NOT CORRECT!!!
 func _on_HandLimits_area_entered(area):
+	#print("area")
 	can_move = false
 	position = pre_position
+	pass # Replace with function body.
+
+
+func _on_HandLimits_area_exited(area):
+	#can_fire = true
 	pass # Replace with function body.
