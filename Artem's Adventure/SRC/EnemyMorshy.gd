3cd6bc74d33681c130394b7b43f5b189293036ab
Gauliux <artyomprokopenko.ru@gmail.com>
2022-11-28 10:19:53 +0300

    HOT UPDATE! Now Morshus can jump! (added demo-evil-scene for Andrew) O_o

--- Artem's Adventure/SRC/EnemyMorshy.gd
+++ Artem's Adventure/SRC/EnemyMorshy.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+export (int) var gravity = 4000
+export (int) var speed = 200
+export (int) var jump_speed = -1800
+
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
 
@@ -11,10 +15,17 @@ func _physics_process(delta):
 	var found_wall = is_on_wall()
 	var found_ledge = not ledgeCheckRight.is_colliding() or not ledgeCheckLeft.is_colliding()
 	
-	if found_wall or found_ledge:
+	if found_wall:
+		velocity.y = jump_speed # now tehy can jump O_o
 		direction *= -1
 	
 	sprite.flip_h = direction.x > 0
 	
-	velocity = direction * 25
-	move_and_slide(velocity, Vector2.UP)
+	velocity.x = 0
+	velocity.x += speed * direction.x
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	#velocity = move_and_slide(velocity, Vector2.UP)
+	#velocity = direction * 25
+	#move_and_slide(velocity, Vector2.UP)
